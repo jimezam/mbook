58e0cfb1c6ec8b6e247226ededae1a1a9052c48a
Jorge I. Meza <jimezam@gmail.com>
2018-10-03 00:20:54 -0500

    Created country migration, model and seeder (data)

---
+++ app/Country.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'english_name', 'iso2', 'iso3', 'phone_prefix',
+    ];
+}
---
+++ database/migrations/2018_10_03_050225_create_countries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('english_name', 50)->nullable();
+            $table->string('iso2', 2);
+            $table->string('iso3', 3);
+            $table->string('phone_prefix', 8)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
