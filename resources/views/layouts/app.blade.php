88df0dd1183ae3de53ab37ace1656f7172bb51d4
Jorge I. Meza <jimezam@autonoma.edu.co>
2018-10-30 18:01:53 -0500

    Added home link to the main menu bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,6 +53,12 @@
                                 @endif
                             </li>
                         @else
+                            <!-- Home -->
+                            <li class="nav-item dropdown">
+                                <a class="nav-link" href="{{ route('home') }}" role="button">
+                                    {{ __('Inicio') }}
+                                </a>
+                            </li>
                             <!-- mBooks -->
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
