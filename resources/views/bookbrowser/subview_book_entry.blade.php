41b1227b9e9570aa3ed99cfbeadab279f22672d8
Jorge I. Meza <jimezam@autonoma.edu.co>
2018-11-01 02:00:59 -0500

    Remove nl2br to book description on browser to avoid very long descriptions

--- resources/views/bookbrowser/subview_book_entry.blade.php
+++ resources/views/bookbrowser/subview_book_entry.blade.php
@@ -28,7 +28,7 @@
             {{ $book->category->name }}
         </h5>
         <p class="card-text text-muted">
-            {{ $book->updated_at->format('M j, Y') }} - {!! nl2br(\Illuminate\Support\Str::words($book->description, 40, ' &hellip;')) !!}
+            {{ $book->updated_at->format('M j, Y') }} - {!! \Illuminate\Support\Str::words($book->description, 40, ' &hellip;') !!}
         </p>
         <a href="{{ route('bookviewer.index', $book->shortname) }}" class="card-link">Leer</a>
         <a href="{{ route('bookviewer.metadata', $book->shortname) }}" class="card-link">Metadatos</a>
