2b1f98f69df66317e9f96f10596e34baedad64c8
Jorge I. Meza <jimezam@autonoma.edu.co>
2018-11-02 21:10:56 -0500

    Improve mbook listing on browser: Editar Libro / Editar Contenidos

--- resources/views/bookbrowser/subview_book_entry.blade.php
+++ resources/views/bookbrowser/subview_book_entry.blade.php
@@ -18,7 +18,7 @@
 <div class="card mb-3">
     <div class="card-body">
         <h3 class="card-title">
-            <a href="{{ route('bookviewer.index', $book->shortname) }}">{{ $book->name }}</a>
+            <a href="{{ route('bookviewer.index', $book->shortname) }}" style="color: black">{{ $book->name }}</a>
             @if($badge != null) 
                 &nbsp;<span class="badge badge-{{ $badgeType }}">{{ __($badge) }}</span>
             @endif   
@@ -33,7 +33,8 @@
         <a href="{{ route('bookviewer.index', $book->shortname) }}" class="card-link">Leer</a>
         <a href="{{ route('bookviewer.metadata', $book->shortname) }}" class="card-link">Metadatos</a>
         @if($book->user_id == Auth::id())
-        <a href="{{ route('mbooks.edit', $book->id) }}" class="card-link">Editar</a>
+        <a href="{{ route('mbooks.edit', $book->id) }}" class="card-link">Editar Libro</a>
+        <a href="{{ route('mbooks.msections.index', $book->id) }}" class="card-link">Editar Contenidos</a>
         @endif
     </div>
 </div>
